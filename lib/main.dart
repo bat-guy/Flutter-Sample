dc106d2aad646d50f0a4e4436e560fad10d8ce55
Dheeraj <parashardheeraj@yahoo.com>
2023-05-30 13:26:47 +0530

    -List with map

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,60 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
-void main() => runApp(MaterialApp(home: MyWidget()));
+void main() => runApp(MaterialApp(home: ListWidget()));
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class ListWidget extends StatefulWidget {
+  const ListWidget({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<ListWidget> createState() => _ListWidgetState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
-  int level = 0;
+class _ListWidgetState extends State<ListWidget> {
+  List<Quotes> quotes = [
+    Quotes(quote: 'aksdhshkadjasdakhsj', author: 'John'),
+    Quotes(quote: 'saudjghsghasdhjkakjs asdhj aksdakhsd sdf', author: 'Joe'),
+    Quotes(quote: 'jhGASdiyeqwfiu iuash ais iaushd ', author: 'Jil')
+  ];
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.indigo[50],
-      appBar: AppBar(
-        title: Text('ID Card'),
-        centerTitle: true,
-        backgroundColor: Colors.indigo,
-        elevation: 0,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            level += 1;
-          });
-        },
-        child: Icon(Icons.add),
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Center(
-              child: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://cdn.vectorstock.com/i/1000x1000/91/07/happy-man-face-vector-6619107.webp'),
-                radius: 40,
-              ),
-            ),
-            Divider(
-              height: 60,
-            ),
             Text(
-              'Name',
-              style: TextStyle(color: Colors.black, letterSpacing: 2),
+              quote.quote,
+              style: TextStyle(fontSize: 18, color: Colors.deepPurple),
             ),
-            SizedBox(height: 10),
+            SizedBox(height: 6),
             Text(
-              'John',
-              style: TextStyle(
-                  color: Colors.green,
-                  letterSpacing: 2,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold),
+              quote.author,
+              style: TextStyle(fontSize: 14, color: Colors.grey),
             ),
-            SizedBox(height: 30),
-            Text(
-              'Current Level',
-              style: TextStyle(color: Colors.black, letterSpacing: 2),
-            ),
-            SizedBox(height: 10),
-            Text(
-              '$level',
-              style: TextStyle(
-                  color: Colors.green,
-                  letterSpacing: 2,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 30),
-            Row(
-              children: [
-                Icon(
-                  Icons.email,
-                  color: Colors.black,
-                ),
-                SizedBox(width: 10),
-                Text(
-                  'abc@abc.com',
-                  style: TextStyle(
-                      color: Colors.black, fontSize: 20, letterSpacing: 1),
-                )
-              ],
-            )
           ],
         ),
       ),
     );
   }
-}
 
-class MyWidget2 extends StatefulWidget {
-  const MyWidget2({super.key});
-
-  @override
-  State<MyWidget2> createState() => _MyWidget2State();
-}
-
-class _MyWidget2State extends State<MyWidget2> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: const Text(
+          'Quotes',
+          style: TextStyle(color: Colors.black54),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.tealAccent,
+      ),
+      body: Column(
+        children: quotes.map((e) => quoteTemplate(e)).toList(),
+      ),
+    );
   }
 }
---
+++ lib/quote.dart
@@ -0,0 +1,6 @@
+class Quotes {
+  String quote;
+  String author;
+
+  Quotes({required this.quote, required this.author});
+}
